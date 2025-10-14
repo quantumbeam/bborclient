dd322a53d72749cba8ea44f482d3ecaf61516c6b
suzukikensuke1 <kensuke_suzuki@ap.eng.osaka-u.ac.jp>
2025-10-14 15:13:00 +0900

    misc: __version__ automatically obtained from pyproject.toml

--- src/bbor_client/__init__.py
+++ src/bbor_client/__init__.py
@@ -1,5 +1,10 @@
+from importlib.metadata import version as get_version
 from .client import BBORClient
 
+
+__version__ = get_version('bbor_client')
 __all__ = [
-    BBORClient,
+    'BBORClient',
+    '__version__',
 ]
+
