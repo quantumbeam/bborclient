df266535935901e27657485dedb6b05fb380ce7f
suzukikensuke1 <kensuke_suzuki@ap.eng.osaka-u.ac.jp>
2025-10-27 10:47:42 +0900

    fix: To read the python project file for the version value, in one hand tomllib is used in the app server, on the other hand importlib.metadata is used for general usages

--- src/bbor_client/__init__.py
+++ src/bbor_client/__init__.py
@@ -1,8 +1,23 @@
-from importlib.metadata import version as get_version
+import os
 from .client import BBORClient
 
+#INFO: tomllib becomes standard with Python>3.11.
+# bbor_client requires 3.9 or over, so __version__ reads pyproject.toml using importlib.
+# But the BBOR server cannot importlib.meta for reading the version value
+# because it does not install the client as a package but setting PYTHONPATH.
+# Therefore, tomllib is used to directly read pyproject.toml
 
-__version__ = get_version('bbor_client')
+try:
+    import tomllib
+    pyproject_path = os.path.join(os.path.dirname(__file__), '../..', 'pyproject.toml')
+    with open(pyproject_path, 'rb') as f:
+        data = tomllib.load(f)
+    __version__ = data['project']['version']
+except ModuleNotFoundError:
+    from importlib.metadata import version as _get_version
+    __version__ = _get_version(__package__ or 'bbor_client')
+
+
 __all__ = [
     'BBORClient',
     '__version__',
